76d16aa38b1ce70878f9a7095ea3a39321df116e
davidbirkin <34802973+davidbirkin@users.noreply.github.com>
2017-12-25 21:26:38 +0000

    Adjusted JSON file format
    Adjusted JSON file format, for some reason when compiling the extension into CRX it is no longer picking up JSON file.

--- js/globalfunction.js
+++ js/globalfunction.js
@@ -1,3 +1,22 @@
+var container = $('div.container');
+
+$('input#get').click(function(){
+    $.ajax({
+        type: 'GET',
+        url: '/js/buildings.json',
+        dataType: 'json',
+        success: function(data) {
+            $.each(data, function(index, item) { 
+                $.each(item, function(key, value) { 
+                    container.append(key + ': ' + value + '<br/>');
+                })
+                container.append('<br/><br/>');
+            });
+        }
+    });
+})
+
+
 $(function () {
     $('#rssdropdown').change(function(){
         $('.resource').hide();
@@ -26,5 +45,3 @@ $(function(){
         $("#lookupupgrades").show();
     });    
 });
-
-
